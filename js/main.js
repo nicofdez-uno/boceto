f98a1cf39cc944828f79505e8b5b392440851161
nico <bichiteca@gmail.com>
2025-03-14 17:41:48 +0100

    Renovatio

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-const searchBox = document.getElementById('searchbox');
+const searchBox = document.querySelector('.searchbox');
 
-const lupa = document.querySelector('svg#search');
+const lupa = document.querySelector('.menu-top .icon');
 
-const hamburguesa = document.querySelector('.icon #hamburguesa');
+// const hamburguesa = document.querySelector('.icon #hamburguesa');
 
-const menuOculto =  document.querySelector('.slideout-menu');
+// const menuOculto =  document.querySelector('.slideout-menu');
 
 lupa.addEventListener('click', function(){
-
- if(getComputedStyle(searchBox).display == 'none'){
-  searchBox.style.display = 'block';
- }else{
-  searchBox.style.display = 'none'
- }
+  console.log('hola')
+  searchBox.classList.toggle('searchbox--show');
+ 
 
 });
 
-hamburguesa.addEventListener('click', function(){
-  if(getComputedStyle(menuOculto).display == 'none'){
-    menuOculto.style.display = 'block';
-    menuOculto.style.pointerEvents = 'auto';
-   }else{
-    menuOculto.style.display = 'none';
-    menuOculto.style.pointerEvents = 'none';
-   }
-});
+// hamburguesa.addEventListener('click', function(){
+//   if(getComputedStyle(menuOculto).display == 'none'){
+//     menuOculto.style.display = 'block';
+//     menuOculto.style.pointerEvents = 'auto';
+//    }else{
+//     menuOculto.style.display = 'none';
+//     menuOculto.style.pointerEvents = 'none';
+//    }
+// });
+
+
+/*-----------menu Dorian-------------*/
+
+const toggleMenuElement = document.querySelector('.top-nav .icon');
+const hiddenMenuElement = document.getElementById('hidden-menu');
+
+toggleMenuElement.addEventListener('click', () => {
+  console.log('hola')
+  hiddenMenuElement.classList.toggle('hidden-menu--show');
+})
